5290b6d666555a14f41578b425aa7983b3a74671
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-06-11 20:40:20 +0200

    Modify and update StaticPage project
    - configuration for *public* diretory content
    - static page middleware(s)

--- StaticPage/Program.cs
+++ StaticPage/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
@@ -16,11 +17,19 @@ namespace StaticPage
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("hosting.json", optional: true, reloadOnChange: true)
+                .AddCommandLine(args)
+                .Build();
+            return Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseConfiguration(config);
                     webBuilder.UseStartup<Startup>();
                 });
+        }
     }
 }
--- StaticPage/Startup.cs
+++ StaticPage/Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -12,29 +13,46 @@ namespace StaticPage
 {
     public class Startup
     {
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // services.AddDirectoryBrowser();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.Use(async (context, next) =>
+            {
+                await next();
+                if (context.Response.StatusCode == StatusCodes.Status404NotFound &&
+                  !Path.HasExtension(context.Request.Path.Value))
+                {
+                    Console.WriteLine("Redirect to /index.html");
+                    context.Request.Path = "/index.html";
+                    await next();
+                }
+                else if (context.Response.StatusCode == StatusCodes.Status404NotFound)
+                {
+                    Console.WriteLine("404 page");
+                    context.Request.Path = "/404.html";
+                    await next();
+                }
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            const string Item = "index.html";
+            DefaultFilesOptions options = new DefaultFilesOptions();
+            options.DefaultFileNames.Clear();
+            options.DefaultFileNames.Add(Item);
+            app.UseDefaultFiles(options);
+            app.UseStaticFiles();
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
         }
     }
 }
